fe10ebda9e53565d707b0ae3f8e0612c6b926a3f
romeopapa <rafal.proski.programista@gmail.com>
2023-09-01 08:05:57 +0200

    Empty project

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Adresowa-seminarium
+//
+//  Created by Rafał Proski on 01/09/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
